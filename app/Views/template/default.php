d3401d5b693e2b8a18ea627b34d1af375f4a91a6
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2022-05-23 12:57:37 +0700

    Update default.php

--- app/Views/template/default.php
+++ app/Views/template/default.php
@@ -39,12 +39,7 @@
   <link rel="preload" as="style" href="<?= base_url('assets/css/owl.carousel.min.css') ?>">
   <link rel="preload" as="style" href="<?= base_url('assets/css/owl.theme.default.min.css') ?>">
   <link rel="preload" as="style" href="<?= base_url('assets/css/magnific-popup.css') ?>">
-  <link rel="preload" as="style" href="<?= base_url('assets/css/aos.css') ?>">
   <link rel="preload" as="style" href="<?= base_url('assets/css/ionicons.min.css') ?>">
-  <link rel="preload" as="style" href="<?= base_url('assets/css/bootstrap-datepicker.css') ?>">
-  <link rel="preload" as="style" href="<?= base_url('assets/css/jquery.timepicker.css') ?>">
-  <link rel="preload" as="style" href="<?= base_url('assets/css/flaticon.css') ?>">
-  <link rel="preload" as="style" href="<?= base_url('assets/css/icomoon.css'); ?>">
   <link rel="preload" as="style" href="<?= base_url('assets/css/style.css'); ?>">
   <link rel="preload" as="style" href="<?= base_url('assets/css/user.css'); ?>">
   <link rel="preload" as="script" href="<?= base_url('assets/js/jquery-migrate-3.0.1.min.js') ?>">
@@ -78,21 +73,11 @@
   <!-- Uses File -->
   <link rel="stylesheet" href="<?= base_url('assets/css/open-iconic-bootstrap.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/animate.css') ?>">
-
   <link rel="stylesheet" href="<?= base_url('assets/css/owl.carousel.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/owl.theme.default.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/magnific-popup.css') ?>">
-
-  <link rel="stylesheet" href="<?= base_url('assets/css/aos.css') ?>">
-
+  <!-- -->
   <link rel="stylesheet" href="<?= base_url('assets/css/ionicons.min.css') ?>">
-
-  <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-datepicker.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/jquery.timepicker.css') ?>">
-
-
-  <link rel="stylesheet" href="<?= base_url('assets/css/flaticon.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('assets/css/icomoon.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
   <link rel="stylesheet" href="<?= base_url('assets/css/user.css'); ?>">
   <script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
